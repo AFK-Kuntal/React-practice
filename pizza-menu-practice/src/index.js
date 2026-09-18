40b99a1deb1e63212221577b6ae327ea60acc7ca
Kuntal <kuntalpanda1@gmail.com>
2026-09-18 17:46:07 +0530

    Refining Codes

--- pizza-menu-practice/src/index.js
+++ pizza-menu-practice/src/index.js
@@ -78,28 +78,41 @@ function Menu() {
     <main className="menu">
       <h2>Menu</h2>
       {num > 0 && ( //num > 0 ->always the condition should return true or false
-        <ul className="pizzas">
-          {pizzas.map(
-            (
-              pizza, //map because it creates new array.using foreach here will not work
-            ) => (
-              <Pizza pizzaInfo={pizza} key={pizza.name} /> //key is to uniquely identify each of the element
-            ),
-          )}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map(
+              (
+                pizza, //map because it creates new array.using foreach here will not work
+              ) => (
+                <Pizza pizzaInfo={pizza} key={pizza.name} /> //key is to uniquely identify each of the element
+              ),
+            )}
+          </ul>
+        </>
       )}
     </main>
   );
+  //In React, components can only return a single root element(main is the root element here). Traditionally, developers wrapped multiple elements in a <div> to satisfy this rule. However, React Fragments solve this problem by acting as an invisible wrapper that disappears once the code is rendered, leaving behind only the child elements.Basically React Fragment allow us to return multiple element without wrapping them inside a div.
 }
 
 function Pizza({ pizzaInfo }) {
   //destructuring the prop->{pizzaInfo},thats way we can immediately know which props we are recieving
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaInfo.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaInfo.photoName} alt={pizzaInfo.name}></img>
       <div>
         <h3>{pizzaInfo.name}</h3>
         <p>{pizzaInfo.ingredients}</p>
+        {/* {pizzaInfo.soldOut ? (
+          <span>"SOLD OUT"</span>
+        ) : (
+          <span>{pizzaInfo.price}</span>
+        )} */}
+        <span>{pizzaInfo.soldOut ? "SOLD OUT" : pizzaInfo.price}</span>
       </div>
     </li>
   );
